75c20a5b8f694aff7cfcac8584dfc376cae24e45
Shruti799 <shrutijwal999@gmail.com>
2024-10-21 20:41:08 +0530

    updated

--- static/js/script.js
+++ static/js/script.js
@@ -58,46 +58,6 @@ function analyze() {
             } else {
                 chartsDiv.innerHTML = '<p>User not found.</p>';
             }
-
-            // if (data.table) {
-            //     tableDiv.innerHTML = data.table;
-            // } else if (data.error) {
-            //     chartsDiv.innerHTML = `<p>${data.error}</p>`;
-            // } else {
-            //     chartsDiv.innerHTML = '<p>User not found.</p>';
-            // }
-
-        //     if (data.table) {
-        //         // Create table
-        //         const table = document.createElement('table');
-        //         table.border = '1';
-
-        //         // Create table header
-        //         const thead = document.createElement('thead');
-        //         const headerRow = document.createElement('tr');
-        //         ['Repository Name', 'Commits', 'Forks', 'Stars'].forEach(header => {
-        //             const th = document.createElement('th');
-        //             th.textContent = header;
-        //             headerRow.appendChild(th);
-        //         });
-        //         thead.appendChild(headerRow);
-        //         table.appendChild(thead);
-
-        //         // Create table body
-        //         const tbody = document.createElement('tbody');
-        //         data.table.forEach(repo => {
-        //             const row = document.createElement('tr');
-        //             [repo.name, repo.commits, repo.forks, repo.stars].forEach(cellData => {
-        //                 const td = document.createElement('td');
-        //                 td.textContent = cellData;
-        //                 row.appendChild(td);
-        //             });
-        //             tbody.appendChild(row);
-        //         });
-        //         table.appendChild(tbody);
-
-        //         tableDiv.appendChild(table);
-        //     }
          })
         .catch(error => {
             const chartsDiv = document.getElementById('charts');
